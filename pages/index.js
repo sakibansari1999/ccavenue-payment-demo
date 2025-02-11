ccdc2183ca5f36d2260f0b7d1ab318b403b62b13
sakibansari1999 <sakib@thor.solutions>
2025-02-11 17:04:07 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Payment() {
 
     let encReq = CCAvenue.getEncryptedOrder(paymentData);
     let accessCode = "AVSR42LL78CE18RSEC";
-    let URL = `https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=${paymentData.merchant_id}6&encRequest=${encReq}&access_code=${accessCode}`;
+    let URL = `https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=${paymentData.merchant_id}6&encRequest=${encReq}&access_code=${accessCode}`;
     router.push(URL);
   };
 
